a266a5a885f1ee59813611e31aad9a068ac050ff
matt@wv8544 <ml881@york.ac.uk>
2015-03-27 23:55:06 +0000

    Put in a test visitor for the safelet and managed to get to the Name of the mission sequencer that it returns.

--- hijactools/trunk/src/java/hijac/tools/application/TightRopeTest.java
+++ hijactools/trunk/src/java/hijac/tools/application/TightRopeTest.java
@@ -2,6 +2,15 @@ package hijac.tools.application;
 
 import freemarker.template.TemplateException;
 
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementVisitor;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Name;
+import javax.lang.model.element.PackageElement;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.TypeParameterElement;
+import javax.lang.model.element.VariableElement;
+
 import hijac.tools.analysis.AnalysisError;
 import hijac.tools.analysis.SCJAnalysis;
 import hijac.tools.analysis.TaskProcessor;
@@ -19,23 +28,81 @@ import hijac.tools.application.UncaughtExceptionHandler;
 import java.io.IOException;
 
 import hijac.tools.modelgen.Output;
+import hijac.tools.modelgen.Target;
 import hijac.tools.modelgen.circus.PostProcessor;
 import hijac.tools.modelgen.circus.SCJApplication;
+import hijac.tools.modelgen.circus.datamodel.ClassModel;
+import hijac.tools.modelgen.circus.datamodel.SafeletModel;
 import hijac.tools.modelgen.circus.translators.CircusTranslator;
 import hijac.tools.modelgen.circus.translators.SafeletTranslator;
+import hijac.tools.modelgen.circus.visitors.AMethodVisitor;
+import hijac.tools.modelgen.targets.ClassTarget;
 import hijac.tools.scjmsafe.translation.*;
 import hijac.tools.scjmsafe.checker.*;
 import hijac.tools.tightrope.translators.Level2Translator;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 import java.util.HashSet;
 
 import javax.lang.model.element.TypeElement;
 
+import com.sun.source.tree.AnnotationTree;
+import com.sun.source.tree.ArrayAccessTree;
+import com.sun.source.tree.ArrayTypeTree;
+import com.sun.source.tree.AssertTree;
+import com.sun.source.tree.AssignmentTree;
+import com.sun.source.tree.BinaryTree;
+import com.sun.source.tree.BlockTree;
+import com.sun.source.tree.BreakTree;
+import com.sun.source.tree.CaseTree;
+import com.sun.source.tree.CatchTree;
 import com.sun.source.tree.ClassTree;
 import com.sun.source.tree.CompilationUnitTree;
+import com.sun.source.tree.CompoundAssignmentTree;
+import com.sun.source.tree.ConditionalExpressionTree;
+import com.sun.source.tree.ContinueTree;
+import com.sun.source.tree.DoWhileLoopTree;
+import com.sun.source.tree.EmptyStatementTree;
+import com.sun.source.tree.EnhancedForLoopTree;
+import com.sun.source.tree.ErroneousTree;
+import com.sun.source.tree.ExpressionStatementTree;
+import com.sun.source.tree.ForLoopTree;
+import com.sun.source.tree.IdentifierTree;
+import com.sun.source.tree.IfTree;
+import com.sun.source.tree.ImportTree;
+import com.sun.source.tree.InstanceOfTree;
+import com.sun.source.tree.LabeledStatementTree;
+import com.sun.source.tree.LiteralTree;
+import com.sun.source.tree.MemberSelectTree;
+import com.sun.source.tree.MethodInvocationTree;
+import com.sun.source.tree.MethodTree;
+import com.sun.source.tree.ModifiersTree;
+import com.sun.source.tree.NewArrayTree;
+import com.sun.source.tree.NewClassTree;
+import com.sun.source.tree.ParameterizedTypeTree;
+import com.sun.source.tree.ParenthesizedTree;
+import com.sun.source.tree.PrimitiveTypeTree;
+import com.sun.source.tree.ReturnTree;
+import com.sun.source.tree.StatementTree;
+import com.sun.source.tree.SwitchTree;
+import com.sun.source.tree.SynchronizedTree;
+import com.sun.source.tree.ThrowTree;
 import com.sun.source.tree.Tree;
+import com.sun.source.tree.TreeVisitor;
+import com.sun.source.tree.TryTree;
+import com.sun.source.tree.TypeCastTree;
+import com.sun.source.tree.TypeParameterTree;
+import com.sun.source.tree.UnaryTree;
+import com.sun.source.tree.UnionTypeTree;
+import com.sun.source.tree.VariableTree;
+import com.sun.source.tree.WhileLoopTree;
+import com.sun.source.tree.WildcardTree;
+import com.sun.source.util.SimpleTreeVisitor;
 import com.sun.source.util.Trees;
+import com.sun.tools.javac.tree.JCTree;
 
 public class TightRopeTest {
    public static SCJAnalysis ANALYSIS;
@@ -49,6 +116,104 @@ public class TightRopeTest {
       Statics.kickstart();
    }
 
+ 
+   
+   public static class SafeletLevel2Visitor implements ElementVisitor<Void,Void>
+   {
+
+	@Override
+	public Void visit(Element e) {
+//		System.out.println(e);
+		return null;
+	}
+
+	@Override
+	public Void visit(Element e, Void p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitExecutable(ExecutableElement e, Void p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitPackage(PackageElement e, Void p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitType(TypeElement e, Void p) {
+		System.out.println(e);
+		
+		ClassTree ct = trees.getTree(e);
+		
+		List<StatementTree> members =  (List<StatementTree>) ct.getMembers();
+		Iterator<StatementTree> i = members.iterator();
+		while (i.hasNext())
+  		  {
+  			  MethodTree o = (MethodTree) i.next();
+  			 System.out.println(o.getName());
+  			 
+  			 if (o.getName().contentEquals("getSequencer"))
+  			 {
+  				 System.out.println("in iterator");
+  				 List<StatementTree> s =  (List<StatementTree>) o.getBody().getStatements();
+  				 
+  				  Iterator j = s.iterator();
+  				  
+  				  while(j.hasNext())
+  				  {
+  					  StatementTree st =  (StatementTree) j.next();
+  					 
+  					  if(st instanceof ReturnTree )
+  					  {
+  						 System.out.println(((ReturnTree) st).getExpression() );
+  						 
+  						Name id = ((IdentifierTree) ((NewClassTree) ((ReturnTree) st).getExpression()).getIdentifier()).getName() ;
+  						 
+  						 System.out.println("Kind: " + id
+  						);
+//  						 ((ReturnTree) st).getExpression()
+  						 
+//  						 st.accept(new ReturnVisitor(), null);
+  						  
+  						//No use this name to get to the next thing I need to explore?
+  						 
+  					  }
+  				  }
+  				 
+  			 }
+  			  
+  		  }
+		
+		
+		return null;
+	}
+
+	@Override
+	public Void visitTypeParameter(TypeParameterElement e, Void p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitUnknown(Element e, Void p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitVariable(VariableElement e, Void p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	   
+   }
+   
    protected static void setUncaughtExceptionHandler() {
       Thread.currentThread().setUncaughtExceptionHandler(
          new UncaughtExceptionHandler());
@@ -77,38 +242,101 @@ public class TightRopeTest {
       units = ANALYSIS.getCompilationUnits();
       type_elements = ANALYSIS.getTypeElements();
       
-     
       
-//      for(TypeElement elem : type_elements) 
-//      {
-////    	  System.out.println(elem.toString());
-//    	  String elemID = elem.toString();
-//    	  if (elemID.equalsIgnoreCase("accs.ACCMission"))
-//    	  {
+     
+      //for all the types in the program
+      for(TypeElement elem : type_elements) 
+      {
+//    	  System.out.println(elem.toString());
+    	  String elemID = elem.toString();
+    	  //if the type we have is the safelet
+    	  if (elemID.equalsIgnoreCase("accs.ACCSafelet"))
+    	  {
+    		 System.out.println("Found Safelet");
+    		  
+    		  
+    		  elem.accept(new SafeletLevel2Visitor(), null);
+    		  
+//    		  SafeletModel sm = new SafeletModel(new SCJApplication(ANALYSIS), new ClassModel(new SCJApplication(ANALYSIS), elem));
+//    		  
+//    		  SafeletTranslator st = new SafeletTranslator();
+//    		  st.setContext(new SCJApplication(ANALYSIS));
+//    		  st.setTarget(new ClassTarget(elem));
+//    		  st.setOutput(new Output());
+//    		  
+//    		  st.execute();
+    		 
+//    		 System.out.println("printing safelet class tree");
+//    		 //get the class tree of the safelet
 //    		  ClassTree tree = trees.getTree(elem);
 //    		  System.out.println(tree.toString());
+//    		     		
 //    		  
-////    		  SafeletTranslator st  = new SafeletTranslator();
-////    		//  st.setTarget(elem);
-////    		  st.invoke();
 //    		  
-//    		 
-////              return super.visitClass(tree, p);
+//    		  System.out.println("Printing members class tree");
+//    		  //this is a sub set of above
+//    		  List<? extends Tree> members =  tree.getMembers();
 //    		  
-////    		  
-//    	  }
+//    		  System.out.println(members.toString());
+//    		  
+//    		  
+//    		  
+//    		  System.out.println("Iterator");
+//    		  Iterator<? extends Tree> i = members.iterator();
+//    		  
+//    		  while (i.hasNext())
+//    		  {
+//    			  MethodTree o = (MethodTree) i.next();
+//    			 System.out.println(o.getName());
+//    			 
+//    			 if (o.getName().contentEquals("getSequencer"))
+//    			 {
+//    				 System.out.println("in iterator");
+//    				 List<? extends StatementTree> s =  o.getBody().getStatements();
+//    				 
+//    				  Iterator j = s.iterator();
+//    				  
+//    				  while(j.hasNext())
+//    				  {
+//    					  StatementTree st = (StatementTree) j.next();
+//    					 
+//    					  if(st instanceof ReturnTree )
+//    					  {
+//    						 System.out.println(((ReturnTree) st).getExpression() );
+//    					  }
+//    				  }
+//    				 
+//    			 }
+//    			  
+//    		  }
+//    	
+    		  
+//    		  SafeletTranslator st  = new SafeletTranslator();
+//    		//  st.setTarget(elem);
+//    		  st.invoke();
+    		  
+    		 
+//              return super.visitClass(tree, p);
+    		  
+//    		  
+    	  }
 //    	  
-//      }
+      }
       
-      Level2Translator circus_trans = new Level2Translator();
-
-      circus_trans.setOutput(new Output());
-
-      circus_trans.execute(new SCJApplication(ANALYSIS));
-
-      PostProcessor post = new PostProcessor();
-
-      post.execute();
+//      Level2Translator circus_trans = new Level2Translator();
+//     
+//      circus_trans.setTarget((Target)t);
+//      
+//      
+//  
+//    
+//      circus_trans.setOutput(new Output());
+//
+//      circus_trans.execute(new SCJApplication(ANALYSIS));
+//
+//      PostProcessor post = new PostProcessor();
+//
+//      post.execute();
 
             
     
