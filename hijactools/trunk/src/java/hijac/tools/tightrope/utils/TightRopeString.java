b602d83c23409bfdc1650f74f9b3a079bfc241f3
matt@wv8544 <ml881@york.ac.uk>
2016-02-09 10:22:42 +0000

    Adding to make sure I've got all the new stuff in.

---
+++ hijactools/trunk/src/java/hijac/tools/tightrope/utils/TightRopeString.java
@@ -0,0 +1,36 @@
+package hijac.tools.tightrope.utils;
+
+public final class TightRopeString
+{
+	public static class LATEX
+	{
+		public static final String DOT = "~.~";
+		public static final String SHRIEK = "~!~";
+		public static final String THEN = "\\then ";
+		public static final String SKIP = "\\Skip";
+		public static final String NEW_LINE = " \\\\ ";
+		public static final String RET_FALSE = "ret := \\false";
+		public static final String RET_TRUE = "ret := \\true";
+		public static final String ASSIGN = " :=";
+		public static final String RPAR = "\\rpar";
+		public static final String RCHANSET = " \\rchanset ";
+		public static final String LPAR = "\\lpar ";
+		public static final String LCHANSET = "\\lchanset ";
+		public static final String INTERLEAVE = "\\interleave";
+		
+	}
+	
+	public static class Name
+	{
+
+		public static final String ID_STR = "ID";
+		public static final String MID = "M"+ID_STR;
+		public static final String SID = "S"+ID_STR;
+		public static final String OBJ_ID = "O"+ID_STR;
+		public static final String Thread_ID = "T"+ID_STR;
+		
+	}
+
+	
+	
+}
